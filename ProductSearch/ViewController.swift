82a99fc002c688b6e87d52eb0d2de03fb4c40853
Jason P Panas <jpanas1@wc320-mac18.local>
2016-03-01 09:22:35 -0700

    Initial Commit

---
+++ ProductSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProductSearch
+//
+//  Created by Jason P Panas on 2016-03-01.
+//  Copyright © 2016 Jason P Panas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
